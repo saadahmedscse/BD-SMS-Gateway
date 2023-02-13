5d0b57cfa7a3422b425b16ca82e7b548ae0ce9b7
saada <saadahmedscse@gmail.com>
2023-02-13 15:48:01 +0600

    removed unused codes

--- app/src/main/java/com/saadahmedsoft/bdsmsgateway/MainActivity.java
+++ app/src/main/java/com/saadahmedsoft/bdsmsgateway/MainActivity.java
@@ -3,11 +3,6 @@ package com.saadahmedsoft.bdsmsgateway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.saadahmedev.bdsmsgateway.gateways.SSLWireless;
-import com.saadahmedev.bdsmsgateway.gateways.SmsQGlobal;
-import com.saadahmedev.bdsmsgateway.interfaces.OnSmsSendListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,29 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SmsQGlobal.getInstance()
-                .setClientId("196c9aa3-7cce-4c3d-b366-2cd5aef5272e")
-                .setApiKey("lYWiNQkhyMR4KwEYkn6jbLcEPeDdW9YHCPgjrNpI7/g=")
-                .setSenderId("TopSeba")
-                .isUnicode(true)
-                .setPhone("+8801794784643")
-                .setMessage("Hello")
-                .send(new OnSmsSendListener() {
-                    @Override
-                    public void onLoading() {
-                        Log.d("sms_debug_tag", "onLoading: ");
-                    }
-
-                    @Override
-                    public void onSuccess() {
-                        Log.d("sms_debug_tag", "onSuccess: ");
-                    }
-
-                    @Override
-                    public void onFailed(String message) {
-                        Log.d("sms_debug_tag", "onFailed: " + message);
-                    }
-                });
     }
 }
