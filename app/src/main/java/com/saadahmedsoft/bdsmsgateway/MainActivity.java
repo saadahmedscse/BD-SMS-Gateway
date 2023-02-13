cd2fbddd5200eaaf8a115beca0638fab422ec57d
saada <saadahmedscse@gmail.com>
2023-02-13 14:22:46 +0600

    Tested ssl wireless functionalities

--- app/src/main/java/com/saadahmedsoft/bdsmsgateway/MainActivity.java
+++ app/src/main/java/com/saadahmedsoft/bdsmsgateway/MainActivity.java
@@ -3,6 +3,10 @@ package com.saadahmedsoft.bdsmsgateway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.saadahmedev.bdsmsgateway.SSLWireless;
+import com.saadahmedev.bdsmsgateway.interfaces.OnSmsSendListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SSLWireless.getInstance()
+                .setApiToken("lu3htwwy-dzadvo6i-wevhole6-gjumi1fj-trejjdar")
+                .setSID("MSOBNON")
+                .setPhone("+8801794784643")
+                .setMessage("This is test message")
+                .setCustomMessageId("sldkflskf")
+                .send(new OnSmsSendListener() {
+                    @Override
+                    public void onLoading() {
+                        Log.d("sms_debug_tag", "onLoading: ");
+                    }
+
+                    @Override
+                    public void onSuccess() {
+                        Log.d("sms_debug_tag", "onSuccess: ");
+                    }
+
+                    @Override
+                    public void onFailed(String message) {
+                        Log.d("sms_debug_tag", "onFailed: " + message);
+                    }
+                });
     }
 }
